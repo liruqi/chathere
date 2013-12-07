73c1a819f4ac54c7dd5164fea846df0f2753c5e1
liruqi <liruqi@gmail.com>
2013-12-07 07:43:16 +0000

    Fix config

--- setting.js
+++ setting.js
@@ -1,14 +1,16 @@
+if (process.env.BAE_ENV_AK) {
 var username = process.env.BAE_ENV_AK;
 var password = process.env.BAE_ENV_SK;
 var db_host = process.env.BAE_ENV_ADDR_SQL_IP;
 var db_port = process.env.BAE_ENV_ADDR_SQL_PORT;
 var db_name = 'TrQJCVxKzyhqiUiGByzN';   //该数据库名为用户在bae平台创建的数据库名称
-
-username = 'root';
-password = '123456';
-db_host = 'localhost';
-db_port = '3306';
-db_name = 'chatroom';
+} else {
+var username = 'root';
+var password = '123456';
+var db_host = 'localhost';
+var db_port = '3306';
+var db_name = 'chatroom';
+}
 
 var option = {
   host: db_host,
