8ca4c66f6f275106e867214ae03b0c1475cc68b4
liruqi <liruqi@gmail.com>
2013-12-07 14:02:49 +0000

    Fix geo detection

--- roomController.js
+++ roomController.js
@@ -1,37 +1,63 @@
+var https=require('https');
 var http=require('http');
-var ak='A5564517ff860a6270ed20e1b85a2bc4';
+
+// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&sensor=false&key=AIzaSyAdoegQZ_IbTtTxLkkhJPOxG9vdcR10ZQs
+
+//                            /maps/api/place/nearbysearch/json?location=116.39564504,39.92998578&radius=500&sensor=false&key=AIzaSyAdoegQZ_IbTtTxLkkhJPOxG9vdcR10ZQs
+var getGooglePlace  = function(latitude, longitude, res) {
+    var path='/maps/api/place/nearbysearch/json?location='+latitude+','+longitude+'&radius=500&sensor=false&key=AIzaSyAdoegQZ_IbTtTxLkkhJPOxG9vdcR10ZQs';
+    var options={
+host:'maps.googleapis.com',
+     path:path,
+     method:'GET'
+    };
+    console.log("Map API:" + path)
+    https.request(options,function(response){
+            var data='';
+            response.on('data',function(chunk){
+                data+=chunk;
+                });
+            response.on('end',function(){
+                var location_json=JSON.parse(data);
+                var pois_json={};
+                pois_json.pois=location_json.results;
+    console.log("Map API:" + JSON.stringify(pois_json));
+                res.end(JSON.stringify(pois_json)); 
+                });
+            }).end(); 
+}
 
 var get=function(req,res){
-        var root_res=res;
-
-        var longitude=req.query.longitude;
-        var latitude=req.query.latitude;
-//        longitude=116.322987;
-  //      latitude=39.983424;
-        var position={
-                longitude:longitude,
-                latitude:latitude
-        };
-        var path='/geocoder/v2/?'+'ak='+ak+'&location='+position.latitude+','+position.longitude+'&output=json&pois=1';
-        var options={
-                host:'api.map.baidu.com',
-                path:path,
-                method:'GET'
-        };
-
-
-        http.request(options,function(res){
+        if (req.query.longitude) {
+            var longitude=req.query.longitude;
+            var latitude=req.query.latitude;
+            getGooglePlace(latitude, longitude, res);
+        } else {
+            var ak='A5564517ff860a6270ed20e1b85a2bc4';
+            var path='/location/ip?'+'ak='+ak+'&ip='+req.connection.remoteAddress+'&coor=bd09ll&output=json';
+            var options = {
+                host: "api.map.baidu.com",
+                path: path,
+                method: 'GET'
+            };
+            console.log("Baidu API PATH:" + path);
+            http.request(options,function(response){
                 var data='';
-                res.on('data',function(chunk){
-                        data+=chunk;
+                response.on('data',function(chunk){
+                    data+=chunk;
                 });
-                res.on('end',function(){
-                        var location_json=JSON.parse(data);
-                        var pois_json={};
-                        pois_json.pois=location_json.result.pois;
-                        root_res.end(JSON.stringify(pois_json)); 
+                response.on('end',function(){
+                    console.log("Baidu API:" + data);
+                    var loc=JSON.parse(data);
+                    if (loc.status == 0) {
+                        getGooglePlace(loc.content.point.y, loc.content.point.x, res);
+                    } else {
+                        res.end(JSON.stringify({code: 1, message: "Cannot locate user", pois: []}));
+                    } 
                 });
-        }).end();
+            }).end();
+        }
+
 };
 
 exports.get=get;
